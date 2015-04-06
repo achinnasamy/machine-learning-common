6b349e9f1474216047f54df6e36699b392d0a1f8
Aravindh Chinnasamy <aravindh.chinnasamy@gmail.com>
2015-04-06 15:34:08 +0530

    Refactoring

--- src/main/java/com/equator/common/constraints/annotations/NotNull.java
+++ src/main/java/com/equator/common/constraints/annotations/NotNull.java
@@ -6,9 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Copyright (c) 2014 iPro,Inc. and its affiliates. All rights reserved.
- */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
